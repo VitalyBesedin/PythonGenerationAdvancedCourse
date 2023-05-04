d452f45f272ec087af6a7c7dedb35f0ca9300982
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-04 18:58:11 +0300

    add first file

---
+++ part_2/less_2_1_1.py
@@ -0,0 +1,11 @@
+# put your python code here
+a, b = int(input()), int(input())
+print(a + b, a - b, a * b, a / b , a // b, a % b,(a ** 10 + b ** 10) ** 0.5, sep='\n' )
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print((a ** 10 + b ** 10) ** 0.5)
