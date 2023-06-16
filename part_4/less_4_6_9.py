25381053a5bb022fde49c8fa704a5f7413967c48
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-06-16 12:35:39 +0300

    Filling with diagonals

--- part_4/less_4_6_9.py
+++ part_4/less_4_6_9.py
@@ -1,9 +1,6 @@
-# filling by snake
+# Filling with diagonals
 n, m = [int(i) for i in input().split()]
-# matrix = [[ i * m + j + 1 for j in range(m)] for i in range(n)]
 
-# for i in range(1,n,2):
-#     matrix[i].reverse()
 lst = [[ 0 for j in range(m)] for i in range(n)]
 nm = 0
 for q in range(n*m):
