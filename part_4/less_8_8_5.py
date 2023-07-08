3ccb608e1115cb4d48d7098a7888e44943eaa05d
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-07-08 12:22:23 +0300

    unique .png filenames

---
+++ part_4/less_8_8_5.py
@@ -0,0 +1,4 @@
+# unique .png filenames
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+files_png ={file.lower() for file in files if file[-4:].lower() == '.png'}
+print(*sorted(files_png))
